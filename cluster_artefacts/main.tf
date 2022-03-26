cf19559e6e0b6ecd2806640eab8a6b3f349f4e21
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2022-03-26 20:37:55 +0530

    Fix namespace.

--- cluster_artefacts/main.tf
+++ cluster_artefacts/main.tf
@@ -219,7 +219,7 @@ resource "helm_release" "loki" {
   repository = "https://grafana.github.io/helm-charts"
   chart      = "loki-stack"
   version    = "2.6.1"
-  namespace  = "loki"
+  namespace  = "logging"
 }
 
 // Sealed secrets
