082545bf090cdad29dbed70de71727abf68eebb6
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2023-09-17 18:41:33 +0530

    For LKE, metrics server needs alterations.

--- cluster_artefacts/metrics-server.tf
+++ cluster_artefacts/metrics-server.tf
@@ -4,4 +4,11 @@ resource "helm_release" "metrics_server" {
   repository = "https://kubernetes-sigs.github.io/metrics-server/"
   version    = "3.10.0"
   count      = var.metrics_server ? 1 : 0
+  values     = <<EOF
+apiService:
+  create: true
+args:
+  - "--kubelet-preferred-address-types=InternalIP"
+  - "--kubelet-insecure-tls"
+EOF
 }
